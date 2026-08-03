e0105f209a758e8e80a82914aafc4a71e27da585
hindochahitarth <hindochahitarth@gmail.com>
2026-08-03 17:35:00 +0530

    Optimized Executor Service

--- Week_Two/Day_Four/SynchronizedDemo.java
+++ Week_Two/Day_Four/SynchronizedDemo.java
@@ -1,6 +1,3 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -17,24 +14,7 @@ public class SynchronizedDemo {
 
     public static void main(String[] args) throws InterruptedException {
 
-        String url = "jdbc:mysql://localhost:3306/your_database"; // change database
-
-        String user = "root";
-        String password = "";
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Connected to MySQL database: " + conn.getCatalog());
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
+        
         Counter counter = new Counter();
 
         ExecutorService executor = Executors.newFixedThreadPool(20); // pool with 2 threads
@@ -53,7 +33,6 @@ public class SynchronizedDemo {
 
         System.out.println(executor.getClass());
 
-        // 5. Wait for all submitted tasks to finish execution (replaces join)
         if (executor.awaitTermination(1, TimeUnit.MINUTES)) {
             System.out.println("Final Count = " + counter.count);
         } else {
